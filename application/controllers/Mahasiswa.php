860ec1443808c6e8c2438978163302c0cdc58739
indah <indahsap00@gmail.com>
2021-04-05 15:06:37 +0700

    menambah fungsi di controller agar tombol tambah, edit, dan hapus data berfungsi sebagaimana mestinya

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -34,4 +34,40 @@ class Mahasiswa extends CI_Controller{
 		$this->m_data->input_data($data,'mahasiswatabel');
 		redirect('Mahasiswa/index');
 	}
+
+	function edit($id){
+	$where = array('id' => $id);
+	$data['mahasiswatabel'] = $this->m_data->edit_data($where,'mahasiswatabel')->result();
+	$this->load->view('v_ubahdata',$data);
+}
+
+function update(){
+	$id = $this->input->post('id');
+	$nama = $this->input->post('nama');
+	$jk = $this->input->post('jk');
+	$agama = $this->input->post('agama');
+	$alamat = $this->input->post('alamat');
+	$tlp = $this->input->post('tlp');
+
+	$data = array(
+		'nama' => $nama,
+		'jk' => $jk,
+		'agama' => $agama,
+		'alamat' => $alamat,
+		'tlp' => $tlp
+	);
+
+	$where = array(
+		'id' => $id
+	);
+
+	$this->m_data->update_data($where,$data,'mahasiswatabel');
+	redirect('Mahasiswa/index');
+}
+
+function hapus($id){
+		$where = array('id' => $id);
+		$this->m_data->hapus_data($where,'mahasiswatabel');
+		redirect('Mahasiswa/index');
+	}
 }
