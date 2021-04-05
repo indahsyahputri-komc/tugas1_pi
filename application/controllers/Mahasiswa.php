6f5e8ac0ed7ade87f20e439aec79e8c883223388
indah <indahsap00@gmail.com>
2021-04-05 14:39:57 +0700

    menambah fungsi di controller agar button tambah data berfungsi

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -2,14 +2,36 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Mahasiswa extends CI_Controller{
  
-	// function __construct(){
-	// 	parent::__construct();		
-	// 	$this->load->model('m_data');
- //                $this->load->helper('url');
-	// }
+	function __construct(){
+		parent::__construct();		
+		$this->load->model('m_data');
+                $this->load->helper('url');
+	}
  
 	function index(){
 		$data['mahasiswatabel'] = $this->m_data->tampil_data()->result();
 		$this->load->view('v_tampilan',$data);
+	}
+
+	function tambah(){
+		$this->load->view('v_tambahdata');
+	}
+
+	function tambah_aksi(){
+		$nama = $this->input->post('nama');
+		$jk = $this->input->post('jk');
+		$agama = $this->input->post('agama');
+		$alamat = $this->input->post('alamat');
+		$tlp = $this->input->post('tlp');
+ 
+		$data = array(
+			'nama' => $nama,
+			'jk' => $jk,
+			'agama' => $agama,
+			'alamat' => $alamat,
+			'tlp' => $tlp
+			);
+		$this->m_data->input_data($data,'mahasiswatabel');
+		redirect('Mahasiswa/index');
 	}
 }
